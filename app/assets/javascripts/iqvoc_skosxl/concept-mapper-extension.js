a743b83f54870004fe9025d7812baff54a9b8ab4
fabianzwodrei <fabian@zwodrei.com>
2018-02-19 16:58:48 +0100

    adds broader path of concepts to mapping autosuggest

---
+++ app/assets/javascripts/iqvoc_skosxl/concept-mapper-extension.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+	"use strict";
+	$(".concept-mapper input.tt-input").each(function(item, node) {
+		$(node.parentElement).bind("typeahead:cursorchanged", function(ev,item){
+			var target_label = item.label;
+
+			var responder = function(data, status, xhr) {
+				var targetSuggestion = $('.tt-cursor:contains('+target_label+')');
+				targetSuggestion.append("<small>" + data["broader_relations_string"] + "</small>")
+				targetSuggestion.addClass('broader_path_added')
+			}
+			if(!$('.tt-cursor:first').hasClass('broader_path_added')) {
+				var loc = window.location
+				var current_language = loc.pathname.split("/")[1]
+				var origin = item.value.split("/")[3]
+				$.getJSON(loc.origin + "/" + current_language + "/concepts/" + origin + "/broader_path", responder);
+			}
+		});
+	});
+});
+
--- app/assets/javascripts/iqvoc_skosxl/manifest.js
+++ app/assets/javascripts/iqvoc_skosxl/manifest.js
@@ -1,2 +1,3 @@
 //= require iqvoc_skosxl/new_label_modal
 //= require iqvoc_skosxl/concept-selector
+//= require iqvoc_skosxl/concept-mapper-extension
