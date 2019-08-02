da30ecb003a9f03744b2fab51df84129c262fdf0
fabianzwodrei <fabian@zwodrei.com>
2019-08-02 15:52:40 +0200

    adds autosuggest for concept-filter in search

--- app/assets/javascripts/iqvoc_skosxl/concept-broader-path-info.js
+++ app/assets/javascripts/iqvoc_skosxl/concept-broader-path-info.js
@@ -25,6 +25,9 @@ jQuery(document).ready(function($) {
   var relation_skos_relateds_input = $('#concept_relation_skos_relateds').parent().find('input.tt-input')[0];
   if (relation_skos_relateds_input) { $(relation_skos_relateds_input).bind("typeahead:cursorchanged", broader_path_info_for_suggestions); }
 
+  var search_filter_broader_concept = $('#search_filter_broader_concept').parent().find('input.tt-input')[0];
+  if (search_filter_broader_concept) { $(search_filter_broader_concept).bind("typeahead:cursorchanged", broader_path_info_for_suggestions); }
+
   	$('.match .concept-mapping-link').each(function(i, e){
 	    var match_target_url = new URL(e.href)
 	    if (match_target_url.origin == window.location.origin) {
