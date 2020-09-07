0d155513723004ff4492f51f552b12211cec1ec6
fabianzwodrei <fabian@zwodrei.com>
2020-09-07 23:19:28 +0200

    overrides bad routing contraints

--- config/initializers/iqvoc_skosxl.rb
+++ config/initializers/iqvoc_skosxl.rb
@@ -1,1 +1,15 @@
 require 'iqvoc/xllabel'
+module Iqvoc
+	module Configuration
+		module Core
+		 	module ClassMethods
+		  		def routing_constraint
+		          lambda do |params, req|
+		            langs = Iqvoc::Concept.pref_labeling_languages.join('|').presence || 'en'
+		            return params[:lang].to_s =~ /^(#{langs})$/
+		          end
+		        end
+		    end
+		end
+	end
+end
